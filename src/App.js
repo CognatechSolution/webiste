4b0bc1eb67c661011e4105514bda1c52ce676758
shubham <shubham.shetyanavar@gmail.com>
2023-01-16 23:57:25 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -19,20 +19,46 @@ import SendIcon from '@mui/icons-material/Send';
 import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
 import Grid from '@mui/material/Grid';
 import SettingsEthernetIcon from '@mui/icons-material/SettingsEthernet';
+import DisplaySettingsIcon from '@mui/icons-material/DisplaySettings';
+import CodeIcon from '@mui/icons-material/Code';
+import CloudSyncIcon from '@mui/icons-material/CloudSync';
+import MediationIcon from '@mui/icons-material/Mediation';
+import StorageIcon from '@mui/icons-material/Storage';
+import VisibilityIcon from '@mui/icons-material/Visibility';
+import useScrollTrigger from '@mui/material/useScrollTrigger';
+import Slide from '@mui/material/Slide';
+import Container from '@mui/material/Container';
+
 
 // import 'animate.css/animate.css' 
 
 
 const drawerWidth = 240;
-const navItems = ['Home', 'About', 'Contact'];
+const navItems = ['About', 'Product', 'Technology', 'Infrastructer', 'Community'];
 
 const theme = createTheme({
   typography: {
     fontFamily: [
       'Open Sans',
     ].join(),
-  },
-});
+  },});
+
+function HideOnScroll(props) {
+  const { children, window } = props;
+  // Note that you normally won't need to set the window ref as useScrollTrigger
+  // will default to window.
+  // This is only being set here because the demo is in an iframe.
+  const trigger = useScrollTrigger({
+    target: window ? window() : undefined,
+  });
+
+  return (
+    <Slide appear={false} direction="down" in={!trigger}>
+      {children}
+    </Slide>
+  );
+}
+
 
 function App(props) {
   const { window } = props;
@@ -44,14 +70,15 @@ function App(props) {
 
   const drawer = (
     <Box onClick={handleDrawerToggle} sx={{ textAlign: 'center' }}>
-      <Typography variant="h6" sx={{ my: 2 }}>
-        MUI
-      </Typography>
+      {/* <Typography variant="h6" sx={{ my: 2 }}>
+        MUI test
+      </Typography> */}
+      <img sx={{  marginRight: '5%' }} src="https://static.wixstatic.com/media/7b17b3_ea39502dd230464aaf2e10bc335d159a~mv2.jpg/v1/fill/w_139,h_43,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/Logo%20(2)_edited.jpg"/>
       <Divider />
       <List>
         {navItems.map((item) => (
           <ListItem key={item} disablePadding>
-            <ListItemButton sx={{ textAlign: 'center' }}>
+            <ListItemButton sx={{ textAlign: 'center', marginRight: '5%' }}>
               <ListItemText primary={item} />
             </ListItemButton>
           </ListItem>
@@ -64,35 +91,42 @@ function App(props) {
 
   return (
     <ThemeProvider theme={theme}>
+      <Box sx={{
+        background: 'rgb(77,31,107)',
+        background: 'linear-gradient(90deg, rgba(77,31,107,1) 15%, rgba(253,29,29,1) 69%, rgba(252,176,69,1) 100%)',
+        textAlign: 'center', color: 'white'
+      }} >
+        <Typography variant="h6caption">
+          Query engine is live! Apply for beta testing here
+        </Typography>
+      </Box>
       <Box sx={{ display: 'flex' }}>
         <CssBaseline />
-        <AppBar component="nav" sx={{ bgcolor: "white", color: 'black' }}>
-          <Toolbar>
-            <IconButton
-              color="inherit"
-              aria-label="open drawer"
-              edge="start"
-              onClick={handleDrawerToggle}
-              sx={{ mr: 2, display: { sm: 'none' } }}
-            >
-              {/* <MenuIcon /> */}
-            </IconButton>
-            <Typography
-              variant="h6"
-              component="div"
-              sx={{ flexGrow: 1, display: { xs: 'none', sm: 'block' } }}
-            >
-              MUI
-            </Typography>
-            <Box sx={{ display: { xs: 'none', sm: 'block' } }}>
-              {navItems.map((item) => (
-                <Button key={item} sx={{ color: 'black' }}>
-                  {item}
-                </Button>
-              ))}
-            </Box>
-          </Toolbar>
-        </AppBar>
+        <HideOnScroll {...props}>
+          <AppBar component="nav" sx={{ bgcolor: "white", color: 'black', marginTop: '30px' }} elevation={1} title={<img src="https://static.wixstatic.com/media/7b17b3_ea39502dd230464aaf2e10bc335d159a~mv2.jpg/v1/fill/w_139,h_43,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/Logo%20(2)_edited.jpg"/>}>
+            <Toolbar>
+              <IconButton
+                color="inherit"
+                aria-label="open drawer"
+                edge="start"
+                onClick={handleDrawerToggle}
+                sx={{ mr: 2, display: { sm: 'none' } }}
+              >
+                {/* <MenuIcon /> */}
+              </IconButton>
+              <img sx={{ marginRight: '9%' }} src="https://static.wixstatic.com/media/7b17b3_ea39502dd230464aaf2e10bc335d159a~mv2.jpg/v1/fill/w_139,h_43,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/Logo%20(2)_edited.jpg" alt="Kitty Katty!" />
+
+              <Box sx={{ display: { xs: 'none', sm: 'block', marginRight: '9%' } }}>
+                {navItems.map((item) => (
+                  <Button key={item} sx={{ color: 'black' }}>
+                    {item}
+                  </Button>
+                ))}
+              </Box>
+            </Toolbar>
+          </AppBar>
+        </HideOnScroll>
+
         <Box component="nav">
           <Drawer
             container={container}
@@ -113,21 +147,24 @@ function App(props) {
         <Box component="main">
           <Toolbar />
           <Box sx={{ padding: '7%', textAlign: 'center', }}>
-            <Typography className='font-header' variant="h1" gutterBottom>
-              All-in-one decentralized data infrastructure.
-            </Typography>
-            <Typography variant="h5">
-              Our mission is to address the information asymmetry in crypto. L3 Atom stores billions of crypto and Web3 data points, transactions, and historical records so anyone can access them without censorship.
-            </Typography>
-            <Button color='success' endIcon={<ArrowForwardIcon />} sx={{ mt: 5 }}>
-              Version 3 is Live!
-            </Button>
+            <Container maxWidth="lg">
+              <Typography className='font-header' variant="h1" gutterBottom>
+                All-in-one decentralized data infrastructure.
+              </Typography>
+              <Typography variant="h5">
+                Our mission is to address the information asymmetry in crypto. L3 Atom stores billions of crypto and Web3 data points, transactions, and historical records so anyone can access them without censorship.
+              </Typography>
+              <Button color='success' endIcon={<ArrowForwardIcon />} sx={{ mt: 5 }}>
+                Version 3 is Live!
+              </Button>
+            </Container>
+
           </Box>
 
           <Box sx={{ textAlign: 'center' }}>
-            <Grid container columns={{ xs: 4, md: 12 }} sx={{ bgcolor: '#ddd' }}>
+            <Grid container columns={{ xs: 4, md: 12 }} sx={{ bgcolor: '#f8f8f8' }}>
               <Grid item xs={12} md={1.7} sx={{ padding: 5 }}>
-                <SettingsEthernetIcon color='primary'/>
+                <SettingsEthernetIcon sx={{ fontSize: '45px' }} color='primary' />
                 <Typography variant="body1" component="p" gutterBottom>
                   Unified API
                 </Typography>
@@ -136,7 +173,7 @@ function App(props) {
                 </Typography>
               </Grid>
               <Grid item xs={12} md={1.7} sx={{ padding: 5 }}>
-              <SettingsEthernetIcon color='primary'/>
+                <DisplaySettingsIcon sx={{ fontSize: '45px' }} color='primary' />
 
                 <Typography variant="body1" component="p" gutterBottom>
                   Power Query
@@ -148,7 +185,7 @@ function App(props) {
                 </Typography>
               </Grid>
               <Grid item xs={12} md={1.7} sx={{ padding: 5 }}>
-              <SettingsEthernetIcon color='primary'/>
+                <CodeIcon sx={{ fontSize: '45px' }} color='primary' />
 
                 <Typography variant="body1" component="p" gutterBottom>
                   Opensource
@@ -160,7 +197,7 @@ function App(props) {
                 </Typography>
               </Grid>
               <Grid item xs={12} md={1.7} sx={{ padding: 5 }}>
-              <SettingsEthernetIcon color='primary'/>
+                <CloudSyncIcon sx={{ fontSize: '45px' }} color='primary' />
 
                 <Typography variant="body1" component="p" gutterBottom>
                   Decentralization
@@ -172,7 +209,7 @@ function App(props) {
                 </Typography>
               </Grid>
               <Grid item xs={12} md={1.7} sx={{ padding: 5 }}>
-              <SettingsEthernetIcon color='primary'/>
+                <MediationIcon sx={{ fontSize: '45px' }} color='primary' />
 
                 <Typography variant="body1" component="p" gutterBottom>
                   Analytics
@@ -184,7 +221,7 @@ function App(props) {
                 </Typography>
               </Grid>
               <Grid item xs={12} md={1.7} sx={{ padding: 5 }}>
-              <SettingsEthernetIcon color='primary'/>
+                <StorageIcon sx={{ fontSize: '45px' }} color='primary' />
 
                 <Typography variant="body1" component="p" gutterBottom>
                   Transparency
@@ -196,7 +233,7 @@ function App(props) {
                 </Typography>
               </Grid>
               <Grid item xs={12} md={1.7} sx={{ padding: 5 }}>
-              <SettingsEthernetIcon color='primary'/>
+                <VisibilityIcon sx={{ fontSize: '45px' }} color='primary' />
 
                 <Typography variant="body1" component="p" gutterBottom>
                   Free data feeds
@@ -210,6 +247,23 @@ function App(props) {
               </Grid>
             </Grid>
           </Box>
+          <Box sx={{ padding: '7%', textAlign: 'center', }}>
+            <Typography variant="h5" sx={{ fontWeight: 'bold' }} gutterBottom>
+              Our vision is to democratize data, address information asymmetry at a scale.
+            </Typography>
+            <Typography variant="h6" gutterBottom>
+              Data & information is the most valuable commodity in the world.
+              Without quality, reliable data all webpages, mobile apps, and the entire internet
+              will stop working.
+            </Typography>
+            <Typography variant="h6">
+              We believe, access to reliable unedited information is a fundamental right, an aspect of a free and
+              democratic society, and the only way to scale crypto & web3. The Core Principles of L3Atom Protocol,
+              by design, has no single authority that can alter its data, how the information is distributed or controlled,
+              or who can see what and when. The infrastructure and entire technology stack is open-source and
+              governed by an open-source community.
+            </Typography>
+          </Box>
         </Box>
       </Box>
     </ThemeProvider>
@@ -218,6 +272,12 @@ function App(props) {
 }
 
 App.propTypes = {
+  /**
+   * Injected by the documentation to work in an iframe.
+   * You won't need it on your project.
+   * 
+   */
+  children: PropTypes.element.isRequired,
   /**
    * Injected by the documentation to work in an iframe.
    * You won't need it on your project.
